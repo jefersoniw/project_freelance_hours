d27b153ae27b05a4e3ee8ba3c7bc104ac6a8ecd0
Jeferson Chagas Silva <jeferson_chagas25@hotmail.com>
2024-10-16 00:24:32 -0300

    feat: Implementação das horas nas propostas, ordenação deccrescente e criando botão para carregar mais propostas caso exista.

--- app/Livewire/Projects/Proposals.php
+++ app/Livewire/Projects/Proposals.php
@@ -3,11 +3,29 @@
 namespace App\Livewire\Projects;
 
 use App\Models\Project;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 
 class Proposals extends Component
 {
     public Project $project;
+
+    public int $qtd = 5;
+
+    #[Computed()]
+    public function proposals()
+    {
+        return $this
+            ->project
+            ->proposals()
+            ->orderBy('hours', 'desc')
+            ->paginate($this->qtd);
+    }
+
+    public function loadMore()
+    {
+        $this->qtd += 10;
+    }
 
     public function render()
     {
